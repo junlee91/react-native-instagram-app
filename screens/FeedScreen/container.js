a5766f513ab5e1e5685471f7c1adc05c8b343356
junlee91 <jlee757@uic.edu>
2018-01-25 23:34:34 -0600

    Making the FeedScreen ( RefreshControl )

--- screens/FeedScreen/container.js
+++ screens/FeedScreen/container.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import FeedScreen from "./presenter";
 
 class Container extends Component {
+  static propTypes = {
+    feed: PropTypes.array,
+    getFeed: PropTypes.func.isRequired
+  };
+  state = {
+    isFetching: false
+  };
+
+  componentWillReceiveProps = nextProps => {
+    if (nextProps.feed) {
+      this.setState({
+        isFetching: false
+      });
+    }
+  };
+
+  _refresh = () => {
+    const { getFeed } = this.props;
+    this.setState({
+      isFetching: true
+    });
+
+    getFeed();
+  };
+
   render() {
-    return <FeedScreen {...this.props} />;
+    return (
+      <FeedScreen {...this.props} {...this.state} refresh={this._refresh} />
+    );
   }
 }
 
