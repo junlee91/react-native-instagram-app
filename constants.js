ccdf1b57c2ebce6c1735b28c01663b9212c777c6
junlee91 <jlee757@uic.edu>
2018-01-28 01:18:13 -0600

    PhotoActions component

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://4bbe8828.ngrok.io"; // to be chaged to new url for ngrok
+export const API_URL = "http://6586c5d4.ngrok.io"; // to be chaged to new url for ngrok
 export const FB_APP_ID = "165241650758972";
 
