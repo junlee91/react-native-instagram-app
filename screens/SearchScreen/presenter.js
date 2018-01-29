1a425f12010ebd754c2ee5167076633d8ba19e43
junlee91 <jlee757@uic.edu>
2018-01-28 20:19:50 -0600

    SearchBar in SearchScreen

---
+++ screens/SearchScreen/presenter.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const SearchScreen = props => <Text>You are searching by {props.searchingBy}</Text>;
+
+export default SearchScreen;
