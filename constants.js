ffbd8c96f4b201ac790091bb68d2c07e7d1d3d87
junlee91 <jlee757@uic.edu>
2018-02-06 23:31:45 -0600

    Picking a Photo from the Library

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://fcb3d34d.ngrok.io"; // to be chaged to new url for ngrok
+export const API_URL = "http://a2142851.ngrok.io"; // to be chaged to new url for ngrok
 export const FB_APP_ID = "165241650758972";
 
